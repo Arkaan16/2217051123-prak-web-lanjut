c2d6b3c7c1283e8eb87122277ebbed5e333d059c
Arkaan16 <112605831+Arkaan16@users.noreply.github.com>
2024-11-12 19:53:13 +0700

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,24 +14,18 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/profile/{nama}/{kelas}/{npm}',[ProfileController::class,'profile']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/user/profile',[UserController::class,'profile']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/user/create',[UserController::class,'create'])->name('user.create');
-
-Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
-
-Route::get('/', [UserController::class, 'index'])->name('user.list');
-
-Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
-
-Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-
-Route::get('/show/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
-
-Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+require __DIR__.'/auth.php';
